a231ccf5fa7bd1668a33758328a1c1b8c27d13b8
Sylwester Kubicki <sylwester.kubicki@gmail.com>
2016-01-06 13:10:08 +0100

    Eigen added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <eigen3/Eigen/Dense>
+#include <iostream>
+using Eigen::MatrixXd;
 
 int main(int argc, char *argv[])
 {
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
